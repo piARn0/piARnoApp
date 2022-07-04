0e4bc08b5f79925a96b1d538bb3c88b695a57268
Hamudi Naanaa <hamudi.naanaa@quartett-mobile.de>
2022-07-04 23:01:34 +0200

    Add (non-functional) initializeActions() function to Engine that declares all actions

--- XrSamples/XrPassthrough/Src/Engine.h
+++ XrSamples/XrPassthrough/Src/Engine.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <openxr/openxr.h>
+#include <array>
 #include "XrPassthroughGl.h"
 #include "Piarno.h"
 
@@ -17,10 +19,28 @@ enum class Mesh : size_t {
     NUM
 };
 
+namespace Side {
+    const int LEFT = 0;
+    const int RIGHT = 1;
+    const int COUNT = 2;
+}  // namespace Side
+
 
 //a layer that abstracts away a lot of the confusing OpenXR implementation details
 //use this class to communicate with the rest of the openxr stuffs
 class Engine {
+    struct InputState {
+        XrActionSet actionSet{XR_NULL_HANDLE};
+        XrAction grabAction{XR_NULL_HANDLE};
+        XrAction poseAction{XR_NULL_HANDLE};
+        XrAction vibrateAction{XR_NULL_HANDLE};
+        XrAction quitAction{XR_NULL_HANDLE};
+        std::array<XrPath, Side::COUNT> handSubactionPath;
+        std::array<XrSpace, Side::COUNT> handSpace;
+        std::array<float, Side::COUNT> handScale = {{1.0f, 1.0f}};
+        std::array<XrBool32, Side::COUNT> handActive;
+    };
+
 public:
     Engine(Scene *scene);
     //getInputState()
@@ -34,9 +54,15 @@ public:
     void update();
     void render();
     static std::vector<Geometry> load_geometries();
+    void initializeActions();
 
 protected:
     Scene *scene;
     Piarno piarno;
-};
 
+    XrInstance m_instance{XR_NULL_HANDLE};
+    XrSession m_session{XR_NULL_HANDLE};
+    XrSystemId m_systemId{XR_NULL_SYSTEM_ID};
+    std::vector<XrView> m_views;
+    InputState m_input;
+};
